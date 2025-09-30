755c4e8f3bd96ef2df4578729cbe4a38c63c7aae
Carole Gérard <gerard.carole78@gmail.com>
2025-09-30 20:59:33 +0200

    Devoir_001

---
+++ src/Bases/Devoir_01.java
@@ -0,0 +1,134 @@
+package Bases;
+
+public class Devoir_01 {
+    public static void main(String[] args) {
+        /*
+            1) Encode dans un tableau les lettres de l'alphabet en minuscule
+         */
+
+        char[] lettres = new char[26];
+
+        //for (int i = 0; i < 26; i++) {
+        //    lettres[i] = (char) ('a' + i);
+        //}
+
+        // OU BIEN
+        for (char c = 'a'; c <= 'z' ; c++) {
+            lettres[c-'a'] = c;
+        }
+
+        //for (int i = 0; i < 26; i++) {
+        //    System.out.println(lettres[i]);
+        //}
+
+
+        /*
+            2) Copie le premier tableau dans un 2è tableau de manière à ce que les lettres
+            soient en majuscules et en sens inverse (de Z -> A)
+         */
+
+        char[] lettresMAJ = new char[lettres.length];
+
+        //for (int i = 0, j = lettres.length - 1; i < lettres.length; i++, j--) {
+        //    lettresMAJ[i] = (char)  (lettres[j] - 32);
+        //}
+
+        //OU BIEN
+        for (int i = lettres.length - 1; i >= 0; i--) {
+            lettresMAJ[lettres.length - 1 - i] = Character.toUpperCase(lettres[i]);
+        }
+
+        //
+
+        //for (int i = 0; i < 26; i++) {
+        //    System.out.println(lettresMAJ[i]);
+        //}
+
+        //for (char c : lettresMAJ) {
+        //    System.out.println(c);
+        //}
+
+
+
+
+        /*
+            3) Créer un tableau à 2 dimensions (10 lignes et 10 colonnes) qui contiendra les nombres de 1 à 100.
+         */
+
+        int[][] tableau = new int[10][10];
+        int k = 1;
+        for (int i = 0; i < tableau.length; i++) {
+            for (int j = 0; j < tableau.length; j++) {
+                tableau[i][j] = k++;
+            }
+        }
+
+        //for (int i = 0; i < tableau.length; i++) {
+        //    for (int j = 0; j < tableau[i].length; j++) {
+        //        System.out.printf("%d ", tableau[i][j]);
+        //    }
+        //    System.out.println();
+        //}
+
+
+
+        /*
+            4) Effacer le numéro 1 et ensuite effacer tous les multiples de chaque nombre que vous trouverez
+         */
+
+        tableau[0][0] = 0;
+        int nbPrimes = 0;
+        for (int i = 0; i < tableau.length; i++) {
+            for (int j = 0; j < tableau[i].length; j++) {
+                if (tableau[i][j] == 0) {
+                    continue;
+                }
+                int m = tableau[i][j];
+                //System.out.println(m);
+                nbPrimes++;
+                for (int l = 0; l < tableau.length; l++) {
+                    for (int n = 0; n < tableau[l].length; n++) {
+                        if (i == l && j == n) continue;
+                        if (tableau[l][n] != 0 && tableau[l][n] % m == 0) {
+                            tableau[l][n] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //for (int i = 0; i < tableau.length; i++) {
+        //    for (int j = 0; j < tableau[i].length; j++) {
+        //        System.out.printf("%d ", tableau[i][j]);
+        //    }
+        //    System.out.println();
+        //}
+
+
+        /*
+            5) Créer un nouveau tableau qui ne contiendra que les nombres restants du tableau précédent
+         */
+
+        int[] tabPrimes = new int[nbPrimes];
+        for (int i = 0, b = 0; i < tableau.length; i++) {
+            for (int j = 0; j < tableau[i].length; j++) {
+                if (tableau[i][j] ==0) {
+                    continue;
+                }
+                tabPrimes[b++] = tableau[i][j];
+
+            }
+        }
+
+
+        /*
+            6) Afficher les nombres premiers inférieures à 100
+         */
+
+        for (int valeur : tabPrimes) {
+            System.out.println(valeur);
+        }
+
+    }
+}
--- src/Bases/Program.java
+++ src/Bases/Program.java
@@ -70,18 +70,18 @@ public class Program {
 
         unInt = 12 + 7 / 5; // 7/5=1.4 mais cela donnera 1
         //-----------------------------------------
-        System.out.println(unInt); // 13
+        //System.out.println(unInt); // 13
         //-----------------------------------------
 
         //var resultat = 12 + 7 / 5.;
         var resultat = 12 + 7 / 5;
         //-----------------------------------------
-        System.out.println(resultat); // sans le "." donne 13 et avec le "." donne 13.4
+        //System.out.println(resultat); // sans le "." donne 13 et avec le "." donne 13.4
         //-----------------------------------------
 
         resultat += 11;
         //-----------------------------------------
-        System.out.println(resultat); // 24.4
+        //System.out.println(resultat); // 24.4 ou 24 selon que les opérations précédentes sont commentées ou non
         //-----------------------------------------
 
         resultat *= 2;
@@ -91,7 +91,7 @@ public class Program {
 
         resultat <<= 2;  // décalage de 2 positions vers la gauche
         //-----------------------------------------
-        System.out.println(resultat); // 192
+        //System.out.println(resultat); // 192
         /*
             00110000 (48)
             << 1 → 01100000 (96)
@@ -102,121 +102,182 @@ public class Program {
         unBool = !unBool;
 
 
-        if (unBool) {
-            System.out.println("unBool est true");
-        } else {
-            System.out.println("unBool est false");
+        //if (unBool) {
+        //    System.out.println("unBool est true");
+        //} else {
+        //    System.out.println("unBool est false");
+        //}
+
+
+        //if (unInt == 1) {
+        //    // RMQ : le == ne fonctionne que pour les nombres, les chaînes de caractères et les booleen
+        //    System.out.println("unInt vaut 1");
+        //}
+
+
+        //if (unInt != 2 && unInt != 3) {
+        //    System.out.println("unInt ne vaut pas 2 ni 3");
+        //}
+
+
+        //if (unInt == 1 || unInt == 2) {
+        //    System.out.println("unInt vaut 1 ou 2");
+        //} else if (unInt == 3 || unInt == 4) {
+        //    System.out.println("unInt vaut 3 ou 4");
+        //}
+
+
+        //switch (unInt) {
+        //    case 1:
+        //        System.out.println("unInt vaut 1");
+        //        break;
+        //    case 2:
+        //        System.out.println("unInt vaut 2");
+        //        break;
+        //    case 4:
+        //        System.out.println("unInt vaut 4");
+        //        break;
+        //    case 3: case 5:
+        //        System.out.println("unInt vaut 3");
+        //        break;
+        //    default:
+        //        System.out.println("unInt ne vaut pas 1, 2, 3 ou 4");
+        //        break;
+        //}
+
+
+        //// RMQ : ne jamais tester == avec un String ! ???? =>...
+        //if (unString == "Bonjour") {
+        //    System.out.println("unString vaut Bonjour");
+        //}
+        //// => À NE JAMAIS FAIRE !!!
+
+
+        //if (5 > unInt) {
+        //    System.out.println("unInt < 5");
+        //}
+
+
+        //// Opérateur ternaire
+        //unInt = (unBool == true ? 1 : 0);
+        //// idem à...
+        //if (unBool) {
+        //    unInt = 1;
+        //} else {
+        //    unInt = 0;
+        //}
+        //// RMQ : utiliser le code le plus lisible et pas le plus élégant !
+
+
+        //while (unInt < 10) {
+        //    System.out.println(unInt++); // de 0 à 9
+        //}
+
+        //while (unInt < 10) {
+        //    System.out.println(++unInt); // de 1 à 10
+        //}
+
+        //while (unInt++ < 10) {
+        //    System.out.println(unInt); // de 1 à 10
+        //}
+
+        //while ((unInt+=2) < 10) {
+        //    System.out.println(unInt); //2 4 6 8
+        //}
+
+
+        //do {
+        //    System.out.println(unInt);
+        //} while (unInt++ < 10);
+
+
+        //for (int i = 0; i < 10; i++) {
+        //    System.out.println(i);
+        //}
+
+
+        //for (int i = 0, j = 2; i < 10; i++, j = j + 2) {
+        //    if (i == 3) continue;
+        //    System.out.println("i = " + i + " - j = " + j);
+        //    if (i == 6) break;
+        //}
+
+
+        //int i = 0;
+        //for (; i++ <= 4 ;) {
+        //    System.out.println(i); // !! de 1 à 5 !!
+        //}
+
+
+        //int[] monTableau = {1, 2, 3, 4, 5};
+        //System.out.println(monTableau[0]);
+        //System.out.println(monTableau.length);
+
+
+        //int[] maTable = new int[10];
+        //System.out.println(maTable[1]);  // 0
+        //maTable[1] = 5;
+        //System.out.println(maTable[1]);  // 5
+
+
+
+        // LES TABLEAUX
+
+        char[][] tableau = new char[5][];
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = new char[5 + i];
+
         }
 
+        //for (int ligne = 0; ligne < tableau.length; ligne++) {
+        //    System.out.printf("Ligne : %d, il y a %d colonnes %n", ligne + 1, tableau[ligne].length);
+        //}
 
-        if (unInt == 1) {
-            // RMQ : le == ne fonctionne que pour les nombres, les chaînes de caractères et les booleen
-            System.out.println("unInt vaut 1");
-        }
-
-
-        if (unInt != 2 && unInt != 3) {
-            System.out.println("unInt ne vaut pas 2 ni 3");
-        }
-
-
-        if (unInt == 1 || unInt == 2) {
-            System.out.println("unInt vaut 1 ou 2");
-        } else if (unInt == 3 || unInt == 4) {
-            System.out.println("unInt vaut 3 ou 4");
-        }
+        //System.out.println(tableau);
+        //int k = 1;
+        //for (int i = 0; i < tableau.length; i++) {
+        //    for (int j = 0; j < tableau[i].length; j++) {
+        //        System.out.println("n° : " + (k) + " /  i = " + i + " /  j = " + j);
+        //        k += 1;
+        //    }
+        //}
+        //System.out.println(tableau.length);
+        //System.out.println(tableau[4].length);
 
 
-        switch (unInt) {
-            case 1:
-                System.out.println("unInt vaut 1");
-                break;
-            case 2:
-                System.out.println("unInt vaut 2");
-                break;
-            case 4:
-                System.out.println("unInt vaut 4");
-                break;
-            case 3: case 5:
-                System.out.println("unInt vaut 3");
-                break;
-            default:
-                System.out.println("unInt ne vaut pas 1, 2, 3 ou 4");
-                break;
-        }
 
+        // LES STRING
 
-//        // RMQ : ne jamais tester == avec un String ! ???? =>...
-//        if (unString == "Bonjour") {
-//            System.out.println("unString vaut Bonjour");
-//        }
-//        // => À NE JAMAIS FAIRE !!!
+        String msg = "Bonjour";
+        System.out.println("msg = " + msg);
+        String texte = "Bonjour";
+        //System.out.println("texte = " + texte);
+        //System.out.println(texte.length());
+        //System.out.println(msg == texte);
 
+        texte = "Coucou";
+        //System.out.println("msg = " + msg);
+        //System.out.println("texte = " + texte);
+        //System.out.println(msg == texte);
 
-//        if (5 > unInt) {
-//            System.out.println("unInt < 5");
-//        }
+        var msg2 = "Bon";
+        var msg3 = "jour";
+        texte = msg2 +msg3;
+        //System.out.println("msg = " + msg);
+        //System.out.println("texte = " + texte);
+        //System.out.println(msg == texte);
 
-        // Opérateur ternaire
-        unInt = (unBool == true ? 1 : 0);
-        // idem à...
-        if (unBool) {
-            unInt = 1;
-        } else {
-            unInt = 0;
-        }
-        // RMQ : utiliser le code le plus lisible et pas le plus élégant !
+        //System.out.println(texte.substring(3));
+        //System.out.println(texte.substring(1, 3));
 
+        //System.out.println(texte.toUpperCase());
+        //System.out.println(texte.toLowerCase());
+        var essai = texte.substring(0, 1).toUpperCase() + texte.substring(1).toLowerCase();
+        //System.out.println("essai = " + essai);
 
-//        while (unInt < 10) {
-//            System.out.println(unInt++); // de 0 à 9
-//        }
+        //System.out.println(essai == "Bonjour"); // false car test la zone mémoire
+        //System.out.println(essai.equals("Bonjour")); // true car test la valeur
 
-//        while (unInt < 10) {
-//            System.out.println(++unInt); // de 1 à 10
-//        }
-
-//        while (unInt++ < 10) {
-//            System.out.println(unInt); // de 1 à 10
-//        }
-
-//        while ((unInt+=2) < 10) {
-//            System.out.println(unInt); //2 4 6 8
-//        }
-
-
-//        do {
-//            System.out.println(unInt);
-//        } while (unInt++ < 10);
-
-
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(i);
-//        }
-
-
-//        for (int i = 0, j = 2; i < 10; i++, j = j + 2) {
-//            if (i == 3) continue;
-//            System.out.println("i = " + i + " - j = " + j);
-//            if (i == 6) break;
-//        }
-
-
-//        int i = 0;
-//        for (; i++ <= 4 ;) {
-//            System.out.println(i); // !! de 1 à 5 !!
-//        }
-
-
-//        int[] monTableau = {1, 2, 3, 4, 5};
-//        System.out.println(monTableau[0]);
-//        System.out.println(monTableau.length);
-
-
-//        int[] maTable = new int[10];
-//        System.out.println(maTable[1]);  // 0
-//        maTable[1] = 5;
-//        System.out.println(maTable[1]);  // 5
 
 
         //-----------------------------------------
